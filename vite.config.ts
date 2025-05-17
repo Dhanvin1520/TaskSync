87d85175cad06394d180f2c555c1bf47332e3aae
Dhanvin⚡️ <Dhanvin@Dhanvins-MacBook-Pro.local>
2025-05-17 23:05:50 +0530

    rerer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src', // 👈 Tells Vite your source files are in `src`
+  build: {
+    outDir: '../dist', // 👈 Output goes to root-level `dist`
+    emptyOutDir: true,
+  }
 })
