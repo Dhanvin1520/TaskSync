1612ed61abadeae52f4b1c1e2458a95432f92d5e
Dhanvin⚡️ <Dhanvin@Dhanvins-MacBook-Pro.local>
2025-05-18 17:48:08 +0530

    hbybg

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://tasksync-1-vxhn.onrender.com/api';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
